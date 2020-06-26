4a2cc0d4340b5882e31a20065e56713d925e41d6
juniozguedes <juniozguedes@gmail.com>
2020-06-25 23:21:03 -0300

    Inserido serializer único para a model Tweet

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
 from .models import Tweet
 
-class TweetListSerializer(serializers.ModelSerializer):
+class TweetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tweet
         fields = '__all__'
-
-class TweetListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tweet
-        fields = '__all__'
-
