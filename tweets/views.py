99a30d309b81f8bb4dac3537616ebd404651aa37
juniozguedes <juniozguedes@gmail.com>
2020-06-25 23:21:34 -0300

    Rota única para criar ou listar tweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import Tweet
-from .serializers import TweetListSerializer
+from .serializers import TweetSerializer
 from rest_framework import generics
-from rest_framework.views import APIView
-
-
-#Return All Tweets
-#class ShowAllTweets(generics.ListAPIView):
-#    serializer_class = TweetListSerializer
-
 
 class ListCreateTweet(generics.ListCreateAPIView):
     def get_serializer_class(self):
         if self.request.method == 'GET':
-            serializer_class = TweetListSerializer    
+            serializer_class = TweetSerializer    
         elif self.request.method == 'POST':
-            serializer_class = TweetListSerializer
+            serializer_class = TweetSerializer
         return serializer_class
     def get_queryset(self):
         queryset = Tweet.objects.all()
