36c436a1eb2a2daa1fcb59e6e17498bb7b51f504
juniozguedes <juniozguedes@gmail.com>
2020-06-26 04:45:11 -0300

    Added retrieve/destroy tweet route

--- tweets/urls.py
+++ tweets/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.ListCreateTweet.as_view(), name='create_tweet'),
-   #path('<pk:>', views.ShowAllTweets.as_view(), name='show_all_tweets'),
-   #path('all', views.ShowAllTweets.as_view(), name='show_all_tweets'),
+    path('', views.ListCreateTweet.as_view(), name='list_create_tweet'),
+    path('<int:pk>', views.RetrieveDestroyTweet.as_view(), name='retrieve_destroy_tweet'),
 ]
