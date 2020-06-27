97e8d429f66ffb8b53c6eff19c944e97be05a92c
juniozguedes <juniozguedes@gmail.com>
2020-06-27 05:04:23 -0300

    Changed tweet field to content in serializer

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class TweetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tweet
-        fields = ['id','tweet','created']
+        fields = ['id','content','created']
