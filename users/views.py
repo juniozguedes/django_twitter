bb33a73361ae844391f9b7a35a030d57b148cb54
juniozguedes <juniozguedes@gmail.com>
2020-06-27 07:10:45 -0300

    Created Follow view

--- users/views.py
+++ users/views.py
@@ -2,15 +2,14 @@ from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_200_OK,
-)
+from rest_framework.status import (HTTP_400_BAD_REQUEST,HTTP_404_NOT_FOUND,HTTP_200_OK,)
 from rest_framework.response import Response
 
-from .serializers import UserSerializer, UserSigninSerializer
+from .serializers import UserSerializer, UserSigninSerializer, FollowSerializer
 from .authentication import token_expire_handler, expires_in
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 
 @api_view(["POST"])
 @permission_classes((AllowAny,))  # here we specify permission by default we set IsAuthenticated
@@ -33,3 +32,9 @@ def signin(request):
         'expires_in': expires_in(token),
         'token': token.key
     }, status=HTTP_200_OK)
+
+class Follow(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated,) 
+    serializer_class = FollowSerializer
+    def perform_create(self, serializer_class):
+        serializer_class.save(follower=self.request.user)
