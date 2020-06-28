4b9402b0ff584ccc1b6af5c15849f81c436fa940
juniozguedes <juniozguedes@gmail.com>
2020-06-28 19:11:13 -0300

    Added proper user_id field as serializer returning user information for the timeline view

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import Tweet
 from django.contrib.auth.models import User
+from users.serializers import UserSerializer
     
 class TweetSerializer(serializers.ModelSerializer):
+    user_id = UserSerializer()
     class Meta:
         model = Tweet
-        fields = ['id','content','created']
+        fields = ['id','user_id','content','created']
