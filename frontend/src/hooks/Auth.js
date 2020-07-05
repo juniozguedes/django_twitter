9e301daaff04520cde120b4ff63799d6fe9cca0f
Filipez <filipezmarron@hotmail.com>
2020-07-05 08:26:19 -0300

    Fix warning

--- frontend/src/hooks/Auth.js
+++ frontend/src/hooks/Auth.js
@@ -31,7 +31,7 @@ const AuthProvider = ({ children }) => {
 
     setData();
     history.push('/login');
-  }, []);
+  }, [history]);
 
   return (
     <AuthContext.Provider value={{ userData: data, signIn, signOut }}>
