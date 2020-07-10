90adce19ab817e3abfcb6f7cfcd7292b07aa6429
Filipez <filipezmarron@hotmail.com>
2020-07-10 17:03:07 -0300

    Change tweet serializer to include favorites

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -3,18 +3,31 @@ from .models import Tweet, Favorites
 
 
 from users.models import User
-from users.serializers import UserSerializer 
 
 from django.contrib.auth.models import User
 from users.serializers import UserSerializer
+
+class TestSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=True)
     
+    favorite_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Favorites
+        fields = [ 'user', 'favorite_count']
+
+    def get_favorite_count(self, obj):
+        return obj.user.count()
+
+
 class TweetSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    favorites = TestSerializer()
+   
+  
     class Meta:
         model = Tweet
-        fields = ('__all__')
-
-
+        fields = ['id','user','content','favorites','created']
 class FavoriteSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=True)
     tweet = TweetSerializer()
